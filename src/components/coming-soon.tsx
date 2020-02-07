71140be55fc58dd534ae1062251c9af7f1683dab
jovanxua <jovanxua@gmail.com>
2020-02-07 13:25:09 +0800

    chore: coming soon

---
+++ src/components/coming-soon.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import styled from '@emotion/styled';
+import Img from "gatsby-image"
+
+export default function ComingSoon() {
+  return (
+    <Container>
+      Coming Soon...
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  position: absolute;
+  flex: 1;
+  height: 100%;
+  width: 100%;
+  font-size: 3.5rem;
+  background-color: #000;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-aw
+`;
